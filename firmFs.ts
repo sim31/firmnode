4a68133bdbc8f4aed5ba7168263ec9b7d8a2d5c7
Tadas <vtadas25@gmail.com>
2023-05-15 11:47:22 +0300

    Listen on filesystem contract for directory change events

--- firmFs.ts
+++ firmFs.ts
@@ -1,9 +1,18 @@
 import { ethers } from 'ethers';
 // TODO: fix path
 import { FirmContractDeployer } from 'firmcontracts/interface/deployer';
+import { Filesystem } from 'firmcontracts/typechain-types';
 
 export default class FirmFs {
   protected _deployer: FirmContractDeployer;
+  private _fsContract: Filesystem | undefined;
+
+  // TODO:
+  // * Save fs contract
+  // * Subscribe to events from fs contract
+  // * Take ipfs through constructor
+  // * Create API (in firmnode) to import files / directories
+  // * Write these files to IPFS
 
   constructor(provider: ethers.providers.JsonRpcProvider) {
     this._deployer = new FirmContractDeployer(provider);
@@ -11,6 +20,13 @@ export default class FirmFs {
 
   async init() {
     await this._deployer.init();
-    await this._deployer.deployFilesystem();
+    this._fsContract = await this._deployer.deployFilesystem();
+
+    this._fsContract.on(
+      this._fsContract.filters.SetRoot(),
+      (addr, cid) => {
+        console.log(`Updating root cid for ${addr}: ${cid}`);
+      }
+    )
   }
 }
